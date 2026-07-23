f7710ca3e074d071001ddad39293fb81f1ee1670
ilyas-11 <ilyassabid11@gmail.com>
2026-07-23 14:50:16 +0100

    Delete target directory

--- target/debug/deps/shell-6ac898a929dea075.d
+++
@@ -1,5 +0,0 @@
-/home/iabid/0-shell/target/debug/deps/shell-6ac898a929dea075.d: src/main.rs
-
-/home/iabid/0-shell/target/debug/deps/shell-6ac898a929dea075: src/main.rs
-
-src/main.rs:
--- target/debug/shell.d
+++
@@ -1,1 +0,0 @@
-/home/iabid/0-shell/target/debug/shell: /home/iabid/0-shell/src/main.rs
