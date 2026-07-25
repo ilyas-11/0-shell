146972bb00c434d7aff0d6cdb190bcb14d49700a
ilyas-11 <ilyassabid11@gmial.com>
2026-07-25 18:34:02 +0100

    add rm command

--- src/main.rs
+++ src/main.rs
@@ -131,6 +131,19 @@ fn main() {
                     eprintln!("mv: {}", err);
                 }
             }
+            "rm" => {
+                if args.is_empty() {
+                    eprintln!("rm: missing operand");
+                } else if args[0] == "-r" {
+                    if args.len() < 2 {
+                        eprintln!("rm: missing operand");
+                    } else if let Err(err) = std::fs::remove_dir_all(args[1]) {
+                        eprintln!("rm: {}", err);
+                    }
+                } else if let Err(err) = std::fs::remove_file(args[0]) {
+                    eprintln!("rm: {}", err);
+                }
+            }
             _ => {
                 println!("Command '{}' not found", command);
             }
