80bdcf8b0d81a4f14a3e2c03ac9d92c8e0908b6d
ilyas-11 <ilyassabid11@gmial.com>
2026-07-25 16:40:18 +0100

    add cp end add mv

--- src/main.rs
+++ src/main.rs
@@ -117,7 +117,20 @@ fn main() {
                     }
                 }
             }
-
+            "cp" => {
+                if args.len() < 2 {
+                    eprintln!("cp: missing operand");
+                } else if let Err(err) = std::fs::copy(args[0], args[1]) {
+                    eprintln!("cp: {}", err);
+                }
+            }
+            "mv" => {
+                if args.len() < 2 {
+                    eprintln!("mv: missing operand");
+                } else if let Err(err) = std::fs::rename(args[0], args[1]) {
+                    eprintln!("mv: {}", err);
+                }
+            }
             _ => {
                 println!("Command '{}' not found", command);
             }
