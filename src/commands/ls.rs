677a616be9ce30dd675b8c39ed6e5f8e2bd238fa
ilyas-11 <ilyassabid11@gmail.com>
2026-07-27 18:07:17 +0100

    Improvement ls to all case

--- src/commands/ls.rs
+++ src/commands/ls.rs
@@ -1,6 +1,8 @@
 use std::fs;
+
 #[cfg(unix)]
 use std::os::unix::fs::PermissionsExt;
+
 #[cfg(unix)]
 fn permissions_to_string(mode: u32) -> String {
     let mut perms = String::new();
@@ -28,53 +30,55 @@ fn permissions_to_string(mode: u32) -> String {
     perms
 }
 
-pub fn  ls(args: &[&str]) {
-    let show_hidden = args.contains(&"-a");
-    let long_format = args.contains(&"-l");
-    let classify = args.contains(&"-F");
-
-    let entries = match fs::read_dir(".") {
+pub fn ls(args: &[&str]) {
+    let mut show_hidden = false;
+    let mut long_format = false;
+    let mut classify = false;
+    let mut path = ".";
+    for arg in args {
+        if arg.starts_with('-') {
+            for ch in arg.chars().skip(1) {
+                match ch {
+                    'a' => show_hidden = true,
+                    'l' => long_format = true,
+                    'F' => classify = true,
+                    _ => {}
+                }
+            }
+        } else {
+            path = arg;
+        }
+    }
+    let entries = match fs::read_dir(path) {
         Ok(entries) => entries,
         Err(err) => {
             eprintln!("ls: {}", err);
             return;
         }
     };
+    let mut entries: Vec<_> = entries.flatten().collect();
+    entries.sort_by_key(|e| e.file_name());
 
     for entry in entries {
-        let entry = match entry {
-            Ok(e) => e,
-            Err(err) => {
-                eprintln!("ls: {}", err);
-                continue;
-            }
-        };
-
         let name = entry.file_name();
         let name = name.to_string_lossy();
-
-        // إخفاء الملفات التي تبدأ بـ '.'
         if !show_hidden && name.starts_with('.') {
             continue;
         }
-
         let metadata = match entry.metadata() {
-            Ok(m) => m,
+            Ok(meta) => meta,
             Err(err) => {
                 eprintln!("ls: {}", err);
                 continue;
             }
         };
-
         let mut display_name = name.to_string();
-
         if classify && metadata.is_dir() {
             display_name.push('/');
         }
-
         if long_format {
             let file_type = if metadata.is_dir() { 'd' } else { '-' };
-            
+
             #[cfg(unix)]
             let permissions = permissions_to_string(metadata.permissions().mode());
 
