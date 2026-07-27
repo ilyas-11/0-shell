43acb712b3112ab0840248329cb1998305204f63
ilyas-11 <ilyassabid11@gmail.com>
2026-07-27 18:02:12 +0100

    Improvement mv

--- src/commands/mv.rs
+++ src/commands/mv.rs
@@ -1,7 +1,20 @@
+use std::path::*;
+
 pub fn mv(args : &[&str]) {
     if args.len() < 2 {
-                    eprintln!("mv: missing operand");
-                } else if let Err(err) = std::fs::rename(args[0], args[1]) {
-                    eprintln!("mv: {}", err);
-                }
+        eprintln!("mv: missing operand");
+
+    }  
+    let source = Path::new(&args[0]);
+    let mut destination = PathBuf::from(&args[1]);
+
+    if destination.is_dir() {
+        if let Some(name) = source.file_name() {
+            destination.push(name);
+        }
+    }
+
+    if let Err(err) = std::fs::rename(source, &destination) {
+        eprintln!("mv: {}", err);
+    }
 }
